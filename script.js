9aaef3ddd10674d5499ee788227e82d2355aa460
Muffin <loan.roze@efrei.net>
2024-01-19 15:16:39 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,28 +1,75 @@
-import data from "./data.json" assert {type : "json"}
-const spentInput = document.getElementById('spentInput')
-const IncomeInput = document.getElementById('incomeInput')
-const spentButton = document.getElementById('spentButton')
-const IncomeButton = document.getElementById('IncomeButton')
-const amountInPage = document.getElementById('amount')
+const spentInput = document.getElementById("spentInput");
+const incomeInput = document.getElementById("incomeInput");
+const spentButton = document.getElementById("spentButton");
+const incomeButton = document.getElementById("incomeButton");
+const setButton = document.getElementById("setButton");
+const setInput = document.getElementById("setInput");
 
-setInterval(() => amountInPage.textContent = data.money, 100)
+const amountInPage = document.getElementById("amount");
+const regEx = /\d*/;
+let moneyData = JSON.parse(localStorage.getItem("money"));
+localStorage.setItem("money", JSON.stringify(moneyData));
+
+setInterval(() => {
+  localStorage.setItem("money", JSON.stringify(moneyData));
+  amountInPage.textContent = moneyData;
+}, 100);
+
+function addMoney(addValue) {
+  if (regEx.test(addValue)) {
+    moneyData += eval(addValue);
+  } else {
+    alert("Please put a value that is only numbers");
+  }
+}
+function decreaseMoney(decValue) {
+  if (regEx.test(decValue)) {
+    moneyData -= eval(decValue);
+  } else {
+    alert("Please put a value that is only numbers");
+  }
+}
+function setMoney(value) {
+  if (regEx.test(value)) {
+    moneyData = eval(value)
+  }
+  else {
+    alert("Please put a value that is only numbers")
+  }
+}
 
 spentButton.addEventListener("click", () => {
-    if (float(spentInput) != float) {
-        alert("Please put a value that is only numbers")
-    }
-    else {
-        data.money += float(spentInput)
-    }
+  decreaseMoney(spentInput.value);
+  spentInput.value = "";
+});
 
-})
+spentInput.addEventListener("keydown", (event) => {
+  if (event.code == "Enter") {
+    decreaseMoney(spentInput.value);
+    spentInput.value = "";
+  }
+});
 
 incomeButton.addEventListener("click", () => {
-    if (float(IncomeInput) != float) {
-        alert("Please put a value that is only numbers")
-    }
-    else {
-        data.money -= float(spentInput)
-    }
-})
+  addMoney(incomeInput.value);
+  incomeInput.value = "";
+});
 
+incomeInput.addEventListener("keydown", (event) => {
+  if (event.code == "Enter") {
+    addMoney(incomeInput.value);
+    incomeInput.value = "";
+  }
+});
+
+setButton.addEventListener("click", () => {
+  setMoney(setInput.value);
+  setInput.value = "";
+});
+
+setInput.addEventListener("keydown", (event) => {
+  if (event.code == "Enter") {
+    setMoney(setInput.value);
+    setInput.value = "";
+  }
+});
