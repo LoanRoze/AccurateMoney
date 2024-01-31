e67177acfab794f85479a2bdc16350773eb5904c
Muffin <loan.roze@efrei.net>
2024-01-31 11:27:26 +0100

    Changes to be committed: 	new file:   historique.html 	modified:   index.html 	new file:   scripts/historique.js 	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,18 +9,31 @@ const setInput = document.getElementById("setInput");
 //variables utiles au code
 const amountInPage = document.getElementById("amount");
 const regEx = /\d*/;
+const regEx2 = "\s*";
+
 //On recupere le tableau stockée dans le localstorage
 let moneyData = JSON.parse(localStorage.getItem("money"));
+let historyData = JSON.parse(localStorage.getItem("history"))
+let currentVersion = 2
 
+//On met a jour en chargeant la page
 updateMoney()
 
 //On verifie si c'est la premiere connexion au site pour le pc, si c'est le cas on initialise moneyData
-if ((moneyData === null)) {
+if ((moneyData === null || moneyData[0].version != currentVersion)) {
   moneyData = [{
     day: new Date().toLocaleDateString(),
     date: Math.floor(Date.now() / 86400000),
-    money: 0
+    money: 0,
+    version: currentVersion
   }]
+  updateMoney()
+}
+
+//De meme pour l'historique
+if (historyData === null) {
+  historyData = []
+  updateHistory()
 }
 
 //On verifie si ça fait plus d'un jour que la derniere connexion au site s'est fait
@@ -29,7 +42,8 @@ if (moneyData[moneyData.length - 1].date != Math.floor(Date.now() / 86400000)) {
     moneyData.push({
         day: new Date().toLocaleDateString(),
         date: Math.floor(Date.now() / 86400000),
-        money: moneyData[moneyData.length - 1].money
+        money: moneyData[moneyData.length - 1].money,
+        version: currentVersion
     })
     //On supprime le json d'il y a 8 jours si besoin
     if (moneyData.length > 8) {
@@ -38,27 +52,61 @@ if (moneyData[moneyData.length - 1].date != Math.floor(Date.now() / 86400000)) {
 }
 
 
-//Les fonctions qu'on appelle quand on appuie sur les boutons
+//Des fonctions qui mettent a jour les données dans le local storage
 function updateMoney() {
   localStorage.setItem("money", JSON.stringify(moneyData));
   amountInPage.textContent = moneyData[moneyData.length - 1].money;
 }
+function updateHistory() {
+  localStorage.setItem("history", JSON.stringify(historyData));
+}
+
+//Les fonctions qu'on appelle quand on appuie sur les boutons
+
+//Augmenter l'argent de addValue
 function addMoney(addValue) {
   if ((regEx.test(addValue)) && (addValue != '')) {
+    let messageText = prompt("Reason to declare :").replace(regEx2)
+    console.log(messageText);
+    if (messageText === "" || messageText.match(regEx2)) {
+      messageText = "Nothing declared"
+    }
+    historyData.push({
+      message: messageText,
+      moneyChange: "+" + addValue,
+      date: new Date().toLocaleDateString()
+    })
+    updateHistory()
+    
     moneyData[moneyData.length - 1].money += eval(addValue);
     updateMoney()
   } else {
     alert("Please put a value that is only numbers");
   }
 }
+
+//Baisser l'argent de decValue
 function decreaseMoney(decValue) {
   if ((regEx.test(decValue)) && (decValue != '')) {
+    let messageText = prompt("Reason to declare :").replace(regEx2)
+    if (messageText === "") {
+      messageText = "Nothing declared"
+    }
+    historyData.push({
+      message: messageText,
+      moneyChange: "-" + decValue,
+      date: new Date().toLocaleDateString()
+    })
+    updateHistory()
+
     moneyData[moneyData.length - 1].money -= eval(decValue);
     updateMoney()
   } else {
     alert("Please put a value that is only numbers");
   }
 }
+
+//Fonction qui met a jour l'argent a value
 function setMoney(value) {
   if ((regEx.test(value)) && (value != '')) {
     moneyData[moneyData.length - 1].money = eval(value);
@@ -69,11 +117,14 @@ function setMoney(value) {
 }
 
 //Les evenements de click sur les boutons
+
+//Bouton Depenses
 spentButton.addEventListener("click", () => {
   decreaseMoney(spentInput.value);
   spentInput.value = "";
 });
 
+//Input Depense (faire l'evenement quand on appuie sur entrer)
 spentInput.addEventListener("keydown", (event) => {
   if (event.code == "Enter") {
     decreaseMoney(spentInput.value);
