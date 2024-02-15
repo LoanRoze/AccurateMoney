591e5312cb9a969dc4348ae2fe88bfeb55e9088e
Muffin <loan.roze@efrei.net>
2024-02-15 20:54:26 +0100

    Changes to be committed: 	modified:   scripts/calculator.js 	modified:   scripts/graphscript.js 	modified:   scripts/historique.js 	modified:   scripts/slate.js 	modified:   scripts/verifile.js

--- scripts/graphscript.js
+++ scripts/graphscript.js
@@ -1,57 +1,60 @@
-const data = JSON.parse(localStorage.getItem("money"))
-const canva = document.getElementById("canvas")
-let moneyList = []
-let daysList = []
+const data = JSON.parse(localStorage.getItem("money"));
+const canva = document.getElementById("canvas");
+let moneyList = [];
+let daysList = [];
 
 for (let index = 0; index < data.length; index++) {
-    moneyList.push(data[index].money);
-    daysList.push(data[index].day)
+  moneyList.push(data[index].money);
+  daysList.push(data[index].day);
 }
 
 console.log(moneyList, daysList);
 
+function createGraph(obj, data, values) {
+  //obj : html item, data : graph values, values : bottom values
 
-function createGraph(obj, data, values) {//obj : html item, data : graph values, values : bottom values
-
-    new Chart(obj, {
-        type: "line",
-        data: {
-            labels: values,
-            datasets: [{
-                data: data,
-                borderColor: "#DAA520",
-                fill: true,
-                label: "money",
-                backgroundColor: "#F0E68C",
+  new Chart(obj, {
+    type: "line",
+    data: {
+      labels: values,
+      datasets: [
+        {
+          data: data,
+          borderColor: "#DAA520",
+          fill: true,
+          label: "money",
+          backgroundColor: "#F0E68C",
+        },
+      ],
+    },
+    options: {
+      legend: { display: true },
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            gridLines: {
+              display: true,
             },
-        ]},
-        options: {
-            legend:{display: true},
-            scales: {
-                xAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: true
-                    },
-                    ticks : {
-                        fontColor: "#2b2a27"
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: true
-                    },
-                    ticks : {
-                        fontColor: "#2b2a27"
-                    }
-                }]
-            }
-        }
-    })
+            ticks: {
+              fontColor: "#2b2a27",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            display: true,
+            gridLines: {
+              display: true,
+            },
+            ticks: {
+              fontColor: "#2b2a27",
+            },
+          },
+        ],
+      },
+    },
+  });
+}
 
-};
-
-
-
-createGraph(canva, moneyList, daysList)
+createGraph(canva, moneyList, daysList);
